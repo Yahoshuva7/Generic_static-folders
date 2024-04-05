bc23e9eb5d72563c6d70ae062fd99b1525ed90d9
Yahoshuva7 <muthakanisiva111@gamil.com>
2024-04-05 18:27:18 +0530

    firstcommit

---
+++ app/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+def styles(request):
+    return render(request,'styles.html')
+def rohit(request):
+    return render(request,'rohit.html')
